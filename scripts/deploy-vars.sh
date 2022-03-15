ff50bc316d233a144cd96c38f08112aed0b81ee4
cheruvu1 <80857514+cheruvu1@users.noreply.github.com>
2022-03-15 09:14:01 -0500

    Create deploy-vars.sh

---
+++ scripts/deploy-vars.sh
@@ -0,0 +1,14 @@
+export DEPLOY_AKS="false"
+export DEPLOY_FLUX="true"
+export USE_KEYVAULT_CERT="false"
+
+# Only used when DEPLOY_AKS is true
+export AZURE_REGION="uksouth"
+export AZURE_RESGRP="bigbang"
+
+# Strongly advise NOT changing these
+export AZURE_DEPLOY_NAME="bigbang"
+export GPG_KEY_NAME="bigbang-sops"
+export NAMESPACE="bigbang"
+export BB_REPO="https://repo1.dso.mil/platform-one/big-bang/bigbang.git"
+export BB_TAG="1.19.0"
